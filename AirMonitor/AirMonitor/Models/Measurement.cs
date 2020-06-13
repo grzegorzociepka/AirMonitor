6879fefbc67ec4daaf934ede4831d2fede98fa7a
Grzesiek <grzegorzociepka09@gmail.com>
2020-06-13 21:35:59 +0200

    i think lab 3 is done

--- AirMonitor/AirMonitor/Models/Measurement.cs
+++ AirMonitor/AirMonitor/Models/Measurement.cs
@@ -1,10 +1,11 @@
-﻿using System;
-namespace AirMonitor.Models
+﻿namespace AirMonitor.Models
 {
     public class Measurement
     {
-        public Measurement()
-        {
-        }
+        public int CurrentDisplayValue { get; set; }
+        public MeasurementItem Current { get; set; }
+        public MeasurementItem[] History { get; set; }
+        public MeasurementItem[] Forecast { get; set; }
+        public Installation Installation { get; set; }
     }
 }
